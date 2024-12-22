68547083f6c75932d354cab7ecebd534fd998e8e
guiller santos <guillersantos111@gmail.com>
2024-12-22 11:19:35 +0800

    Initial Commit

--- NTS.Client/obj/Debug/net8.0/NTS.Client.AssemblyInfo.cs
+++ NTS.Client/obj/Debug/net8.0/NTS.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NTS.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca064be6bc61c40bb56168d84743a7cc044d3f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("NTS.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NTS.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
