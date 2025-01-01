ec526c53233afc7499cd52cb0dc04b9488a1539a
Guiller Santos <guillersantos111@gmail.com>
2025-01-01 16:09:19 +0800

    Added ConnectionString and API Endpoints Security

--- NTS.Server/Program.cs
+++ NTS.Server/Program.cs
@@ -1,12 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Swashbuckle.AspNetCore.Filters;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using NTS.Server.Database.DatabaseContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<WebAppDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 
 builder.Services.AddCors(policy =>
@@ -14,11 +49,12 @@ builder.Services.AddCors(policy =>
     policy.AddPolicy("AllowAllOrigins", policy =>
     {
         policy
-        .WithOrigins("https://localhost:7095","http://localhost:5022")
+        .WithOrigins("https://localhost:7095", "http://localhost:5022")
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
 });
+
 
 var app = builder.Build();
 
