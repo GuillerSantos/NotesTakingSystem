1c9857a1a095ca2844facbf8b5a5193c7a37089f
Guiller Santos <guillersantos111@gmail.com>
2025-01-01 15:45:01 +0800

    Added User Entity

---
+++ NTS.Server/Domain/Entities/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace NTS.Server.Domain.Entities
+{
+    public class Users
+    {
+        public Guid UserId { get; set; } = Guid.NewGuid();
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? RecoveryEmail { get; set; }
+        public DateTime? DateJoined { get; set; } = DateTime.UtcNow;
+    }
+}
