8641cddadca80aedebd634da45bcfbe004dff489
Guiller Santos <guillersantos111@gmail.com>
2025-01-02 10:25:44 +0800

    Modified AuthService cuz i remove the AuthRepository

---
+++ NTS.Server/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTS.Server.Database.DatabaseContext;
+using NTS.Server.Domain.DTOs;
+using NTS.Server.Domain.Entities;
+using NTS.Server.Securities;
+using NTS.Server.Services.Contracts;
+
+namespace NTS.Server.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly WebAppDBContext dBContext;
+        private readonly TokenAndPasswordHash tokenAndPasswordHash;
+        private readonly IConfiguration configuration;
+
+        public AuthService(WebAppDBContext dBContext, TokenAndPasswordHash tokenAndPasswordHash, IConfiguration configuration)
+        {
+            this.dBContext = dBContext;
+            this.tokenAndPasswordHash = tokenAndPasswordHash;
+            this.configuration = configuration;
+        }
+    }
+}
---
+++ NTS.Server/Services/Contracts/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using NTS.Server.Domain.DTOs;
+using NTS.Server.Domain.Entities;
+
+namespace NTS.Server.Services.Contracts
+{
+    public interface IAuthService
+    {
+
+    }
+}
