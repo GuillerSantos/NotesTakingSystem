457820e2418a0607910ba5520613e0a0f8fee548
Guiller Santos <guillersantos111@gmail.com>
2025-01-02 10:21:25 +0800

    UserSignUpDto, UserLoginDto and ForgotPassworDto

---
+++ NTS.Server/Domain/DTOs/ForgotPasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace NTS.Server.Domain.DTOs
+{
+    public class ForgotPasswordDto
+    {
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
---
+++ NTS.Server/Domain/DTOs/UserSignUpDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTS.Server.Domain.DTOs
+{
+    public class UserSignUpDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string? PhoneNumber { get; set; }
+
+        [Required]
+        public string? RecoveryEmail { get; set; }
+    }
+}
