17fc5ebb5eca700296c1c7c4697c72232734ddda
Guiller Santos <guillersantos111@gmail.com>
2025-01-03 15:20:18 +0800

    Added UsersDto for Method GetAllUsersAccounts

---
+++ NTS.Server/Domain/DTOs/UsersDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NTS.Server.Domain.DTOs
+{
+    public class UsersDto
+    {
+        public Guid UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateTime? DateJoined { get; set; }
+        public string Role { get; set; }
+    }
+}
