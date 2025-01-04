eca89e8cb68d126b4828482deb6fcaaba4992b11
Guiller Santos <guillersantos111@gmail.com>
2025-01-04 19:41:36 +0800

    Added Email Settings Model

---
+++ NTS.Server/Domain/Models/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace NTS.Server.Domain.Models
+{
+    public class EmailSettings
+    {
+        public string SMTPServer { get; set; }
+        public int SMTPPort { get; set; }
+        public string FromEmail { get; set; }
+        public string AppPassword { get; set; }
+        public string FromName { get; set; }
+    }
+}
