e35eb833bd85fa1dc59a266451b9b37b3c95f52a
Guiller Santos <guillersantos111@gmail.com>
2025-01-05 22:06:05 +0800

    Added Login Response DTO

---
+++ NTS.Client/Domain/DTOs/LoginResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NTS.Client.Domain.DTOs
+{
+    public class LoginResponseDto
+    {
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+    }
+}
