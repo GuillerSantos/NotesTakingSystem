1b97a38a275a8ac36d708a6a0ef53d2ca2adcdca
guiller santos <guillersantos111@gmail.com>
2025-01-12 22:42:48 +0800

    Added NotesDTO

---
+++ NTS.Server/Domain/DTOs/NotesDto.cs
@@ -0,0 +1,13 @@
+﻿namespace NTS.Server.Domain.DTOs
+{
+    public class NotesDto
+    {
+        public Guid NoteId { get; set; }
+        public Guid UserID { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty ;
+        public string Priority { get; set; } = string.Empty;
+        public bool IsPublic { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
