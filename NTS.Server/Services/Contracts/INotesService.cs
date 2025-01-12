5398770a4cd8ef756edfc760b6136bc16b65af0c
guiller santos <guillersantos111@gmail.com>
2025-01-12 22:41:17 +0800

    Added Contract for NotesService

---
+++ NTS.Server/Services/Contracts/INotesService.cs
@@ -0,0 +1,13 @@
+﻿using NTS.Server.Domain.DTOs;
+using NTS.Server.Domain.Entities;
+
+namespace NTS.Server.Services.Contracts
+{
+    public interface INotesService
+    {
+        Task<Notes> GetNotesByIdAsync(Guid noteId);
+        Task<Notes?> CreateNoteAsync(NotesDto note, Guid userId);
+        Task UpdateNoteAsync(NotesDto note);
+        Task<bool> RemoveNoteAsync(Guid noteId);
+    }
+}
