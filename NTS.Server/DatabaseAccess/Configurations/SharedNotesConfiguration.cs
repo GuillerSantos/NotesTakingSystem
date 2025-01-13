9f3e4d8415511cece7f7649e57223d4331761563
guiller santos <guillersantos111@gmail.com>
2025-01-13 21:00:18 +0800

    Added New Configurations

---
+++ NTS.Server/DatabaseAccess/Configurations/SharedNotesConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NTS.Server.Domain.Entities;
+
+namespace NTS.Server.Database_Access.Configurations
+{
+    public class SharedNotesConfiguration : IEntityTypeConfiguration<SharedNotes>
+    {
+        public void Configure(EntityTypeBuilder<SharedNotes> entityBuilder)
+        {
+            entityBuilder
+                .ToTable("SharedNotes");
+
+            entityBuilder
+                .HasKey(s => s.SharedNoteId);
+
+            entityBuilder
+                .HasOne(s => s.Notes)
+                .WithMany()
+                .HasForeignKey(s => s.NoteId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            entityBuilder
+                .HasOne(s => s.Users)
+                .WithMany()
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entityBuilder
+                .Property(s => s.CreatedAt)
+                .IsRequired();
+
+            entityBuilder
+                .HasIndex(s => s.NoteId)
+                .HasDatabaseName("IX_FavoriteNotes_NoteId");
+
+            entityBuilder
+                .HasIndex(s => s.UserId)
+                .HasDatabaseName("IX_FavoriteNotes_UserId");
+        }
+    }
+}
---
+++ NTS.Server/DatabaseAccess/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NTS.Server.Database.Configurations;
+using NTS.Server.Database_Access.Configurations;
+using NTS.Server.Domain.Entities;
+
+namespace NTS.Server.Database.DatabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<ApplicationUsers> Users { get; set; }
+        public DbSet<Notes> Notes { get; set; }
+        public DbSet<FavoriteNotes> FavoriteNotes { get; set; }
+        public DbSet<ImportantNotes> ImportantNotes { get; set; }
+        public DbSet<SharedNotes> SharedNotes { get; set; }
+        public DbSet<StarredNotes> StarredNotes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new UsersConfiguration());
+            builder.ApplyConfiguration(new NotesConfiguration());
+            builder.ApplyConfiguration(new FavoriteNotesConfiguration());
+            builder.ApplyConfiguration(new ImportantNotesConfiguration());
+            builder.ApplyConfiguration(new SharedNotesConfiguration());
+            builder.ApplyConfiguration(new StarredNotesConfiguration());
+        }
+    }
+}
