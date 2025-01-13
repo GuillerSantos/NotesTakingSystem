9afc509a816a629f8afc6a1cd6f50c634920038d
guiller santos <guillersantos111@gmail.com>
2025-01-13 21:10:36 +0800

    Added new Endpoints for NotesController

--- NTS.Server/Controllers/NotesController.cs
+++ NTS.Server/Controllers/NotesController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NTS.Server.Domain.DTOs;
 using NTS.Server.Domain.Entities;
 using NTS.Server.Services.Contracts;
+using System.Security.Claims;
 
 namespace NTS.Server.Controllers
 {
@@ -17,12 +19,14 @@ namespace NTS.Server.Controllers
             this.notesService = notesService;
         }
 
-        [HttpPost("create-note")]
+        [HttpPost("create-note"), Authorize (Roles = "User")]
         public async Task<IActionResult> CreateNotesAsync([FromBody] NotesDto request)
         {
             try
             {
-                Guid userId = Guid.NewGuid();
+                Guid userId = Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value ??
+                    throw new Exception("UserId not found in token"));
+    
 
                 var response = await notesService.CreateNoteAsync(request, userId);
 
