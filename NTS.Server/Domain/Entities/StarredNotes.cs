b50a1dd6d18c0055df17d218f4d1dc11d8b78529
guiller santos <guillersantos111@gmail.com>
2025-01-13 20:47:49 +0800

    Added StarredNotesEntity

---
+++ NTS.Server/Domain/Entities/StarredNotes.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NTS.Server.Domain.Entities
+{
+    public class StarredNotes
+    {
+        [Key]
+        public Guid StarredNotesId { get; set; }
+
+        [ForeignKey("NoteId")]
+        public Guid NoteId { get; set; }
+
+        [ForeignKey("UserId")]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        public Notes Notes { get; set; }
+
+        public ApplicationUsers Users { get; set; }
+    }
+}
