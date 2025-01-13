1a53dee767ce269b083b6c51fd0ea6f4778a9458
guiller santos <guillersantos111@gmail.com>
2025-01-13 21:09:25 +0800

    Updated NotesDto

--- NTS.Server/Domain/DTOs/NotesDto.cs
+++ NTS.Server/Domain/DTOs/NotesDto.cs
@@ -2,12 +2,10 @@
 {
     public class NotesDto
     {
-        public Guid NoteId { get; set; }
-        public Guid UserID { get; set; }
+        public Guid UserId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty ;
         public string Priority { get; set; } = string.Empty;
-        public bool IsPublic { get; set; }
         public DateTime? CreatedAt { get; set; }
     }
 }
