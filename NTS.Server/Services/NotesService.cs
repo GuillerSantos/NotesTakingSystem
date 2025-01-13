59b2099fee99dd9e0b69973ec31c26b265331975
guiller santos <guillersantos111@gmail.com>
2025-01-13 21:07:03 +0800

    Added new Methods for NotesService

--- NTS.Server/Services/NotesService.cs
+++ NTS.Server/Services/NotesService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using NTS.Server.Database.DatabaseContext;
 using NTS.Server.Domain.DTOs;
 using NTS.Server.Domain.Entities;
@@ -21,62 +22,181 @@ namespace NTS.Server.Services
             {
                 var note = new Notes 
                 {
+                    UserId = userId,
                     Title = request.Title,
                     Content = request.Content,
-                    Priority = MapPriorityColorToPriorityString(request.Priority),
-                    IsPublic = request.IsPublic,
-                    CreatedAt = request.CreatedAt,
-                    UserId = userId,
+                    Priority = request.Priority,
+                    CreatedAt = DateTime.UtcNow
                 };
+
                 dbContext.Notes.Add(note);
                 await dbContext.SaveChangesAsync();
-
                 return note;
             }
             catch (Exception ex)
             {
-                throw new Exception($"Error Creating Notes: {ex.Message}");
+                throw new Exception($"Error Creating Note: {ex.Message}");
             }
         }
 
 
-        public async Task<Notes> GetNotesByIdAsync(Guid noteId)
+        public async Task<Notes> EditNotesAsync( EditNotesDto editNotesDto, Guid noteId, Guid userId)
         {
-            return await dbContext.Notes.FindAsync(noteId);
+            try
+            {
+                var existingNote = await dbContext.Notes.FindAsync(editNotesDto.NoteId);
+                if (existingNote == null || existingNote.UserId != userId) return null;
+
+                existingNote.Title = editNotesDto.Title;
+                existingNote.Content = editNotesDto.Content;
+                existingNote.Priority = editNotesDto.Priority;
+
+                dbContext.Notes.Update(existingNote);
+                await dbContext.SaveChangesAsync();
+                return existingNote;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error Editing Note: {ex.Message}");
+            }
         }
+
 
         public async Task<bool> RemoveNoteAsync(Guid noteId)
         {
-            var note = await dbContext.Notes.FindAsync(noteId);
-
-            if (note == null)
+            try
             {
-                return false;
-            }
+                var note = await dbContext.Notes.FindAsync(noteId);
+                if (note == null) return false;
 
-            dbContext.Notes.Remove(note);
+                dbContext.Remove(note);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error Removing Note: {ex.Message}");
+            }
+        }
+
+
+        public async Task<List<Notes>> GetAllNotesAsync(Guid userId)
+        {
+            return await dbContext.Notes.Where(n => n.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Notes> GetNoteByIdAsync(Guid noteId, Guid userId)
+        {
+            var note = await dbContext.Notes.FindAsync(noteId);
+            if (note == null || (note.UserId != userId && !dbContext.SharedNotes.Any(sn => sn.NoteId == noteId && sn.UserId == userId)))
+                return null;
+
+            return note;
+        }
+
+
+        public async Task<IQueryable<Notes>> SearchNotesAsync(string searchTerm)
+        {
+            var notes = dbContext.Notes.Where(n => n.Title.Contains(searchTerm) || n.Content.Contains(searchTerm));
+            return notes;
+        }
+
+
+        public async Task<bool> MarkNoteAsFavoriteAsync(Guid noteId, Guid userId)
+        {
+            var note = await dbContext.Notes.FindAsync(noteId);
+            if (note == null || note.UserId != userId) return false;
+
+            var favoriteNote = new FavoriteNotes
+            {
+                FavoriteNoteId = Guid.NewGuid(),
+                NoteId = noteId,
+                UserId = userId,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            dbContext.FavoriteNotes.Add(favoriteNote);
             await dbContext.SaveChangesAsync();
             return true;
         }
 
-        
-        public Task UpdateNoteAsync(NotesDto note)
+
+        public async Task<bool> MarkNoteAsImportantAsync(Guid noteId, Guid userId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var note = await dbContext.Notes.FindAsync(noteId);
+                if (note == null || note.UserId != userId) return false;
+
+                var importantNote = new ImportantNotes
+                {
+                    ImportantNoteId = Guid.NewGuid(),
+                    NoteId = noteId,
+                    UserId = userId,
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                dbContext.ImportantNotes.Add(importantNote);
+                await dbContext.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error Marking The Note: {ex.Message}");
+            }
         }
 
-        private string MapPriorityColorToPriorityString(string priorityColor)
+
+        public async Task<bool> MarkNoteAsSharedAsync(Guid noteId, Guid userId, Guid sharedWithUserId)
         {
-            switch (priorityColor.ToLower())
+            try
             {
-                case "red":
-                    return "High";
-                case "yellow":
-                    return "Low";
-                case "green":
-                    return "Normal";
-                default:
-                    return "Normal";
+                var note = await dbContext.Notes.FindAsync(noteId);
+                if (note == null || note.UserId != userId) return false;
+
+                var sharedNote = new SharedNotes
+                {
+                    SharedNoteId = Guid.NewGuid(),
+                    NoteId = noteId,
+                    UserId = userId,
+                    SharedWithUserId = sharedWithUserId,
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                dbContext.SharedNotes.Add(sharedNote);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error Marking Note: {ex.Message}");
+            }
+        }
+
+
+        public async Task<bool> MarkNoteAsStarredAsync(Guid noteId, Guid userId)
+        {
+            try
+            {
+                var note = await dbContext.Notes.FindAsync(noteId);
+                if (note == null || note.UserId != userId) return false;
+
+                var starredNote = new StarredNotes
+                {
+                    StarredNotesId = Guid.NewGuid(),
+                    NoteId = noteId,
+                    UserId = userId,
+                    CreatedAt = DateTime.UtcNow
+                };
+
+                dbContext.StarredNotes.Add(starredNote);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error Marking Note: {ex.Message}");
             }
         }
     }
