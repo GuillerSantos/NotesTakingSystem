1a68d14d75dac0e8ce1421daad3d40701a486464
guiller santos <guillersantos111@gmail.com>
2025-01-17 21:54:38 +0800

    Rename Notes Entity to NotesBase

---
+++ NTS.Server/Entities/NotesBase.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NTS.Server.Entities
+{
+    public class NotesBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid NoteId { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(ApplicationUser))]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(20)]
+        public string Priority { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ApplicationUsers ApplicationUser { get; set; }
+    }
+}
