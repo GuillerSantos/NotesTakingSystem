da2c9a67446ff8a05ff46b3239a17e149f18b02e
guiller santos <guillersantos111@gmail.com>
2025-01-21 22:27:29 +0800

    Updated MainLayout

---
+++ NTS.Client/Shared/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using NTS.Client.Services.Contracts;
+using YourApp.Client.Securities;
+
+namespace NTS.Client.Shared
+{
+    public partial class MainLayout
+    {
+        public bool drawerOpen = false;
+
+        [Inject] NavigationManager navigationManager { get; set; }
+        [Inject] IAuthService authService { get; set; }
+
+        public void ToggleDrawer()
+        {
+            this.drawerOpen = !drawerOpen;
+        }
+
+        public async Task LogoutAsync()
+        {
+            await authService.LogoutAsync();
+            navigationManager.NavigateTo("/", true);
+        }
+    }
+}
