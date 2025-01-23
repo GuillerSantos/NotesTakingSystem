625cc2a77cf8ab6485736955a97fe7a96ad3a4c9
guiller santos <guillersantos111@gmail.com>
2025-01-23 20:30:35 +0800

    Added NoteDto F\or Creating and Displaying Notes

---
+++ NTS.Client/Models/DTOs/NoteDto.cs
@@ -0,0 +1,11 @@
+﻿namespace NTS.Client.Models.DTOs
+{
+    public class NoteDto
+    {
+        public Guid NoteId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Priority { get; set; } = "Normal";
+        public DateTime CreatedAt { get; set; }
+    }
+}
