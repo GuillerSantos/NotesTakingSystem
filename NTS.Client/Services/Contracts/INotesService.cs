e09a9a6266dabcde3baa45d6c8a46f5ca56ed357
guiller santos <guillersantos111@gmail.com>
2025-01-23 20:32:57 +0800

    Added New Request Method

--- NTS.Client/Services/Contracts/INotesService.cs
+++ NTS.Client/Services/Contracts/INotesService.cs
@@ -1,6 +1,11 @@
-﻿namespace NTS.Client.Services.Contracts
+﻿using NTS.Client.Models.DTOs;
+
+namespace NTS.Client.Services.Contracts
 {
     public interface INotesService
     {
+        Task<List<NoteDto>> GetAllNotesAsync();
+        Task CreateNoteAsync(NoteDto request);
+        Task UpdateNoteAsync(NoteDto request, Guid noteId);
     }
 }
