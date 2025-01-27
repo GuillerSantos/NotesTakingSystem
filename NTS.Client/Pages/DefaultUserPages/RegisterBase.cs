baf483bec9c180fe17e684a9f4a3d04ed04cb8d1
guiller santos <guillersantos111@gmail.com>
2025-01-27 19:52:40 +0800

    Added Register User

---
+++ NTS.Client/Pages/DefaultUserPages/RegisterBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using NTS.Client.Models.DTOs;
+using NTS.Client.Services;
+using NTS.Client.Services.Contracts;
+
+namespace NTS.Client.Pages.DefaultUserPages
+{
+    public class RegisterBase : ComponentBase
+    {
+        [Inject] public IAuthService authService { get; set; }
+
+        public RegisterDto register { get; set; } = new RegisterDto();
+
+        public async Task HandleRegisterAsync()
+        {
+            try
+            {
+                var response = await authService.RegisterDefaultUserAsync(register);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error Registrating Account: {error.Message}");
+            }
+        }
+    }
+}
