1f048388111c8fa59f0dd1620e17394629514afb
guiller santos <guillersantos111@gmail.com>
2025-01-27 19:55:27 +0800

    Added FavoriteNotesService

---
+++ NTS.Client/Services/Contracts/IFavoriteNotesService.cs
@@ -0,0 +1,9 @@
+﻿using NTS.Client.Models;
+
+namespace NTS.Client.Services.Contracts
+{
+    public interface IFavoriteNotesService
+    {
+        Task MarkAsFavoriteNoteAsync(FavoriteNotes request, Guid noteId);
+    }
+}
