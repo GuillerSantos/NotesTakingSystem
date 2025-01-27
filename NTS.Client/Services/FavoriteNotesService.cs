65c5e56fa0c058279b929daa429238564dbae6d9
guiller santos <guillersantos111@gmail.com>
2025-01-27 19:58:49 +0800

    No Changes Yet

---
+++ NTS.Client/Services/FavoriteNotesService.cs
@@ -0,0 +1,28 @@
+﻿using NTS.Client.Models;
+using NTS.Client.Services.Contracts;
+
+namespace NTS.Client.Services
+{
+    public class FavoriteNotesService : IFavoriteNotesService
+    {
+        private readonly HttpClient httpClient;
+
+        public FavoriteNotesService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task MarkAsFavoriteNoteAsync(FavoriteNotes request, Guid noteId)
+        {
+            try
+            {
+                var response = await httpClient.PostAsJsonAsync($"/apiFavoriteNote/mark-favoritenote/{noteId}", request);
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error Marking Note As Favorite: {error.Message}");
+            }
+        }
+    }
+}
