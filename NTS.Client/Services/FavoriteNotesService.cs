71e831509bc40c051409f9d8ac74959621b59821
guiller santos <guillersantos111@gmail.com>
2025-01-29 21:24:34 +0800

    Added UnmarkAsFavoriteNote

--- NTS.Client/Services/FavoriteNotesService.cs
+++ NTS.Client/Services/FavoriteNotesService.cs
@@ -6,23 +6,46 @@ namespace NTS.Client.Services
     public class FavoriteNotesService : IFavoriteNotesService
     {
         private readonly HttpClient httpClient;
+        private readonly ILogger<FavoriteNotesService> logger;
 
-        public FavoriteNotesService(HttpClient httpClient)
+        public FavoriteNotesService(HttpClient httpClient, ILogger<FavoriteNotesService> logger)
         {
             this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            this.logger = logger;
         }
+
 
         public async Task MarkAsFavoriteNoteAsync(FavoriteNotes request, Guid noteId)
         {
             try
             {
-                var response = await httpClient.PostAsJsonAsync($"/apiFavoriteNote/mark-favoritenote/{noteId}", request);
+                var response = await httpClient.PostAsJsonAsync($"/api/FavoriteNotes/mark-favorite/{noteId}", request);
                 response.EnsureSuccessStatusCode();
             }
             catch (Exception error)
             {
-                Console.WriteLine($"Error Marking Note As Favorite: {error.Message}");
+                logger.LogError($"Error Marking Note As Favorite: {error.Message}");
             }
+        }
+
+
+        public async Task UnmarkNoteAsFavoriteNoteAsync(Guid noteId, Guid userId)
+        {
+            try
+            {
+                var response = await httpClient.DeleteAsync($"/api/FavoriteNotes/unmark-favoritenote/{noteId}/{userId}");
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception error)
+            {
+                logger.LogError($"Error Unmarking Note As Favorite: {error.Message}");
+            }
+        }
+
+
+        public async Task GetAllFavoriteNotesAsync()
+        {
+            
         }
     }
 }
