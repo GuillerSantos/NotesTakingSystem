f49887b35263130193cf2ec45118961cc7f974d8
guiller santos <guillersantos111@gmail.com>
2025-01-30 21:12:01 +0800

    Adding Search Functionality

--- NTS.Client/Services/Contracts/INotesService.cs
+++ NTS.Client/Services/Contracts/INotesService.cs
@@ -5,6 +5,7 @@ namespace NTS.Client.Services.Contracts
     public interface INotesService
     {
         Task<List<NoteDto>> GetAllNotesAsync();
+        Task<List<NoteDto>> SearchNotesAsync(string searchQuery);
         Task CreateNoteAsync(NoteDto request);
         Task UpdateNoteAsync(NoteDto request, Guid noteId);
         Task RemoveNoteAsync(Guid noteId);
