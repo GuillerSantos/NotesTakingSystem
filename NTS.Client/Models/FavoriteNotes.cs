07a3483d60728cf76fd9f163a3ade784fa0c8698
guiller santos <guillersantos111@gmail.com>
2025-01-31 22:17:37 +0800

    Added Content Property

--- NTS.Client/Models/FavoriteNotes.cs
+++ NTS.Client/Models/FavoriteNotes.cs
@@ -4,6 +4,7 @@
     {
         public Guid FavoriteNoteId { get; set; }
         public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
         public Guid NoteId { get; set; }
         public Guid UserId { get; set; }
         public DateTime CreatedAt { get; set; }
