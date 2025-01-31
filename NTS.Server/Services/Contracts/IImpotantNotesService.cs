050f2690f4e34d0c801afd3579538013b09327aa
guiller santos <guillersantos111@gmail.com>
2025-01-31 21:59:33 +0800

    Added GetAllImportantNotes Method

--- NTS.Server/Services/Contracts/IImpotantNotesService.cs
+++ NTS.Server/Services/Contracts/IImpotantNotesService.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
+using NTS.Server.Entities;
 
 namespace NTS.Server.Services.Contracts
 {
     public interface IImpotantNotesService
     {
         Task<bool> MarkNoteAsImportantAsync(Guid noteId, Guid userId);
-
+        Task<List<ImportantNotes>> GetAllImportantNotesAsync(Guid userId);
         Task RemoveByNoteIdAsync(Guid noteId);
     }
 }
