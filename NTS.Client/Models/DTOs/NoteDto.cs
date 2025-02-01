55879c58e94bb3e7cd0929ebe6efd214ca3c4d22
guiller santos <guillersantos111@gmail.com>
2025-02-01 18:18:38 +0800

    Added Annotaions in Title and Content Properties

--- NTS.Client/Models/DTOs/NoteDto.cs
+++ NTS.Client/Models/DTOs/NoteDto.cs
@@ -1,12 +1,23 @@
-﻿namespace NTS.Client.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTS.Client.Models.DTOs
 {
     public class NoteDto
     {
         public Guid NoteId { get; set; }
+
         public Guid UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
         public string Content { get; set; } = string.Empty;
+
         public string Color { get; set; } = string.Empty;
+
         public DateTime CreatedAt { get; set; }
     }
 }
