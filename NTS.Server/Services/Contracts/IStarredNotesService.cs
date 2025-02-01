f844bb797634606bba2fe42457e8d5769471cb0d
guiller santos <guillersantos111@gmail.com>
2025-02-01 18:12:04 +0800

    Added GetAllStarredNotesAsync and Modified Unmark Note

--- NTS.Server/Services/Contracts/IStarredNotesService.cs
+++ NTS.Server/Services/Contracts/IStarredNotesService.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
+using NTS.Server.Entities;
 
 namespace NTS.Server.Services.Contracts
 {
     public interface IStarredNotesService
     {
         Task<bool> MarkNoteAsStarredAsync(Guid noteId, Guid userId);
-
-        Task RemoveByNoteIdAsync(Guid noteId);
+        Task<List<StarredNotes>> GetAllStarredNotesAsync(Guid userId);
+        Task<bool> UnmarkNoteAsStarredAsync(Guid noteId);
     }
 }
