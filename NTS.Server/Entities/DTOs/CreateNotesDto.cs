e58702b03c7eba93f89d8a93e730121d250a0496
guiller santos <guillersantos111@gmail.com>
2025-02-01 18:33:17 +0800

    Added Annotations

--- NTS.Server/Entities/DTOs/CreateNotesDto.cs
+++ NTS.Server/Entities/DTOs/CreateNotesDto.cs
@@ -4,8 +4,12 @@ namespace NTS.Server.Entities.DTOs
 {
     public class CreateNotesDto
     {
+        [Required]
+        [StringLength(50)]
         public string Title { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(1000)]
         public string Content { get; set; } = string.Empty;
 
         public string Color { get; set; } = string.Empty;
