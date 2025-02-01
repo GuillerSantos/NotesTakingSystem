fcf3e5889b5cc6bf7b388699601adefa9f35a193
guiller santos <guillersantos111@gmail.com>
2025-02-01 18:21:34 +0800

    Added Load All Starred Notes

--- NTS.Client/Pages/DefaultUserPages/BookMarkBase.cs
+++ NTS.Client/Pages/DefaultUserPages/BookMarkBase.cs
@@ -9,39 +9,45 @@ namespace NTS.Client.Pages.DefaultUserPages
     {
         [Inject] public IFavoriteNotesService favoriteNotesService { get; set; }
         [Inject] public IImportantNotesService importantNotesService { get; set; }
+        [Inject] public IStarredNotesService starredNotesService { get; set; }
 
-
-        public NoteDto note { get; set; } = new NoteDto();
         public List<ImportantNotes> importantNotes { get; set; }
         public List<FavoriteNotes> favoriteNotes { get; set; }
+        public List<StarredNotes> starredNotes { get; set; }
+        public NoteDto note { get; set; } = new NoteDto();
         public bool isFetched = false;
 
         protected override async Task OnInitializedAsync()
         {
-            await LoadFavoriteNotesASYNC();
+            await LoadFavoriteNotesAsync();
             isFetched = true;
         }
 
-        public async Task LoadFavoriteNotesASYNC()
+        public async Task LoadFavoriteNotesAsync()
         {
             try
             {
                 var favoriteNotesTask = favoriteNotesService.GetAllFavoriteNotesAsync();
                 var importantNotesTask = importantNotesService.GetAllImportantNotesAsync();
+                var starredNotesTask = starredNotesService.GetAllStarredNotesAsync();
 
-                await Task.WhenAll(favoriteNotesTask, importantNotesTask);
+                await Task.WhenAll(favoriteNotesTask, importantNotesTask, starredNotesTask);
 
                 favoriteNotes = favoriteNotesTask.Result?.ToList() ?? new List<FavoriteNotes>();
                 importantNotes = importantNotesTask.Result?.ToList() ?? new List<ImportantNotes>();
+                starredNotes = starredNotesTask.Result?.ToList() ?? new List<StarredNotes>();
 
-                if (favoriteNotes == null)
+                if (favoriteNotes is null && importantNotes is null && starredNotes is null)
                 {
-                    Console.WriteLine("GetAllFavoriteNotesAsync Returned Null");
                     favoriteNotes = new List<FavoriteNotes>();
+                    importantNotes = new List<ImportantNotes>();
+                    starredNotes = new List<StarredNotes>();
                 }
                 else
                 {
                     favoriteNotes = favoriteNotes.ToList();
+                    importantNotes = importantNotes.ToList();
+                    starredNotes = starredNotes.ToList();
                 }
 
                 isFetched = true;
