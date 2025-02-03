fad5c11a6ef551bc02d7ef68e234d91c90a4f116
guiller santos <guillersantos111@gmail.com>
2025-02-03 20:53:01 +0800

    Implement GetAllSharedNotesAsync Interface

--- NTS.Server/Services/Contracts/ISharedNotesService.cs
+++ NTS.Server/Services/Contracts/ISharedNotesService.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
+using NTS.Server.Entities;
 
 namespace NTS.Server.Services.Contracts
 {
     public interface ISharedNotesService
     {
         Task<bool> MarkNoteAsSharedAsync(Guid noteId, Guid userId);
-
+        Task<List<SharedNotes>> GetAllSharedNotesAsync(Guid userId);
         Task UnmarkNoteAsSharedAsync(Guid noteId);
     }
 }
