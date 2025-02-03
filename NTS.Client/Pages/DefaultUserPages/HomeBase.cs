5381b91cef71a51bf12a1948bd418904b40d5552
guiller santos <guillersantos111@gmail.com>
2025-02-03 20:56:37 +0800

    Renamed Dashboard to Home

---
+++ NTS.Client/Pages/DefaultUserPages/HomeBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using NTS.Client.Services.Contracts;
+
+namespace NTS.Client.Pages.DefaultUserPages
+{
+    public class HomeBase : ComponentBase
+    {
+        [Inject] private ISharedNotesService sharedNotesService { get; set; } = default!;
+    
+    
+
+    }
+}
