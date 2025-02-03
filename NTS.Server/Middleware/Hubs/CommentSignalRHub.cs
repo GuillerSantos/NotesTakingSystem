f0ccdae3c7d61b148580f99cc57f005eb6d3f9e3
guiller santos <guillersantos111@gmail.com>
2025-02-03 20:47:59 +0800

    Added Signal R Hub, Prepering to use Signal R

---
+++ NTS.Server/Middleware/Hubs/CommentSignalRHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace NTS.Server.Middleware.Hubs
+{
+    public class CommentSignalRHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
