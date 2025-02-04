36dc1e57c890b50217ff3f2a0ccd73ddde3edfa6
guiller santos <guillersantos111@gmail.com>
2025-02-04 21:01:25 +0800

    Added Comment Entity

---
+++ NTS.Server/Entities/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTS.Server.Entities
+{
+    public class Comment
+    {
+        [Key]
+        public Guid CommentId { get; set; }
+        public Guid NoteId { get; set; }
+        public Guid UserId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(300)]
+        public string CommentContent { get; set; } = string.Empty;
+
+        [StringLength(50), Required]
+        public string FullName { get; set; } = string.Empty;
+
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+        public ApplicationUsers Users { get; set; } = default!;
+        public Notes Notes { get; set; } = default!;
+    }
+}
