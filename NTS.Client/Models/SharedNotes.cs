0d14b5ce8e25f29c2fb2d9707963ffeba8e3629b
guiller santos <guillersantos111@gmail.com>
2025-02-04 20:56:55 +0800

    Added FullName Property

--- NTS.Client/Models/SharedNotes.cs
+++ NTS.Client/Models/SharedNotes.cs
@@ -4,9 +4,10 @@
     {
         public Guid FavoriteNoteId { get; set; }
         public string Title { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
         public Guid NoteId { get; set; }
         public Guid UserId { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } =DateTime.Now;
         public string Color { get; set; } = string.Empty;
     }
 }
