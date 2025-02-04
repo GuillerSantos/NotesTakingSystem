88da7c17de649347b48d300fcb862bc18d74470d
guiller santos <guillersantos111@gmail.com>
2025-02-04 21:05:01 +0800

    Implementing GetComments

---
+++ NTS.Server/Services/CommentsService.cs
@@ -0,0 +1,25 @@
+﻿using NTS.Server.Data;
+using NTS.Server.Entities;
+using Microsoft.EntityFrameworkCore;
+using NTS.Server.Services.Contracts;
+
+namespace NTS.Server.Services
+{
+    public class CommentsService : ICommentsService
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CommentsService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Comment>> GetCommentsForNoteAsync(Guid noteId)
+        {
+            return await _dbContext.Comments
+                                    .Where(c => c.NoteId == noteId)
+                                    .OrderBy(c => c.CreateAt)
+                                    .ToListAsync();
+        }
+    }
+}
