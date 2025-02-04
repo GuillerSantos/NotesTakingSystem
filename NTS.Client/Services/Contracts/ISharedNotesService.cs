68de3cfe4b3f2ff642a4d5ae73511493e88aa5e5
guiller santos <guillersantos111@gmail.com>
2025-02-04 21:19:26 +0800

    Implementing Unmark Note Method Request

--- NTS.Client/Services/Contracts/ISharedNotesService.cs
+++ NTS.Client/Services/Contracts/ISharedNotesService.cs
@@ -5,6 +5,7 @@ namespace NTS.Client.Services.Contracts
     public interface ISharedNotesService
     {
         Task MarkNoteAsSharedAsync(SharedNotes request, Guid noteId);
-        Task<List<SharedNotes>> GetAllSharedNotesAsync();
+        Task UnmarkNoteAsSharedAsync(Guid noteId);
+        Task<List<SharedNotes>> GetAllSharedNotesAsync(Guid userId);
     }
 }
