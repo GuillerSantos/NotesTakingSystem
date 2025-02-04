bc2d0d44f0ac569028d3d2845ec4ca8a1d694fee
guiller santos <guillersantos111@gmail.com>
2025-02-04 21:04:26 +0800

    Implementing GetComments

---
+++ NTS.Server/Services/Contracts/ICommentsService.cs
@@ -0,0 +1,9 @@
+﻿using NTS.Server.Entities;
+
+namespace NTS.Server.Services.Contracts
+{
+    public interface ICommentsService
+    {
+        Task<List<Comment>> GetCommentsForNoteAsync(Guid noteId);
+    }
+}
