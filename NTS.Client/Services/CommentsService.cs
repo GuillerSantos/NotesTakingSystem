4c5017758def0840e4db6c319b147da1b5c3bb4d
guiller santos <guillersantos111@gmail.com>
2025-02-04 21:11:43 +0800

    Implementing GetCommets

---
+++ NTS.Client/Services/CommentsService.cs
@@ -0,0 +1,25 @@
+﻿using NTS.Client.Models;
+using NTS.Client.Services.Contracts;
+using System.Net.Http;
+
+namespace NTS.Client.Services
+{
+    public class CommentsService : ICommentsService
+    {
+        private readonly HttpClient httpClient;
+
+        public CommentsService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<List<Comment>> GetCommentsForNoteAsync(Guid noteId)
+        {
+            var response = await httpClient.GetAsync($"/api/get-comments/{noteId}");
+            response.EnsureSuccessStatusCode();
+
+            var comments = await response.Content.ReadFromJsonAsync<List<Comment>>();
+            return comments ?? new List<Comment>();
+        }
+    }
+}
