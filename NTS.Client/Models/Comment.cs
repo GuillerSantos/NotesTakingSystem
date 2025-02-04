d33f2e8474012b1b98cf16b8a72f6461a5c09929
guiller santos <guillersantos111@gmail.com>
2025-02-04 20:58:48 +0800

    Added Comment Model

---
+++ NTS.Client/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace NTS.Client.Models
+{
+    public class Comment
+    {
+        public Guid CommentId { get; set; }
+        public Guid NoteId { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string CommentContent { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
