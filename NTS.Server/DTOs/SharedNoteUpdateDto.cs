8b415ddc5ed123df89254c784bec7a025146ac4d
guiller santos <guillersantos111@gmail.com>
2025-02-05 20:15:17 +0800

    Added DTO For NoteUpdate

---
+++ NTS.Server/DTOs/SharedNoteUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace NTS.Server.DTOs
+{
+    public class SharedNoteUpdateDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+    }
+}
