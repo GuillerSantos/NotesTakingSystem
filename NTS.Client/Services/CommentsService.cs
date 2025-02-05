dcf08dd42a8a37da00410991be54f131a2c96ee6
guiller santos <guillersantos111@gmail.com>
2025-02-05 20:19:55 +0800

    Fixing Comment Request Methods

--- NTS.Client/Services/CommentsService.cs
+++ NTS.Client/Services/CommentsService.cs
@@ -1,6 +1,7 @@
 ﻿using NTS.Client.Models;
 using NTS.Client.Services.Contracts;
 using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace NTS.Client.Services
 {
@@ -13,6 +14,8 @@ namespace NTS.Client.Services
             this.httpClient = httpClient;
         }
 
+        public event Action<Guid, Guid, string, string, string, DateTime> OnCommentReceived;
+
         public async Task<List<Comment>> GetCommentsForNoteAsync(Guid noteId)
         {
             var response = await httpClient.GetAsync($"/api/get-comments/{noteId}");
@@ -20,6 +23,32 @@ namespace NTS.Client.Services
 
             var comments = await response.Content.ReadFromJsonAsync<List<Comment>>();
             return comments ?? new List<Comment>();
+        }
+
+        public async Task SendCommentAsync(Guid noteId, Guid userId, string title, string fullName, DateTime createdAt, string content)
+        {
+            var comment = new Comment
+            {
+                NoteId = noteId,
+                UserId = userId,
+                Title = title,
+                FullName = fullName,
+                CreatedAt = createdAt,
+                Content = content
+            };
+
+            var response = await httpClient.PostAsJsonAsync("/api/send-comment", comment);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                OnCommentReceived?.Invoke(noteId, userId, title, fullName, content, createdAt);
+            }
+        }
+
+        public Task StartAsync()
+        {
+            throw new NotImplementedException("StartAsync logic is not yet implemented.");
         }
     }
 }
