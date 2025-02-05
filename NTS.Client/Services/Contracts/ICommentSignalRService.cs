22c4331f725b869ce4f3a775db786d396225c736
guiller santos <guillersantos111@gmail.com>
2025-02-05 20:14:44 +0800

    Added Contacts for CommentSignalRService.cs

---
+++ NTS.Client/Services/Contracts/ICommentSignalRService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NTS.Client.Services.Contracts
+{
+    public interface ICommentSignalRService
+    {
+        event Action<Guid, Guid, string, string, string, DateTime> OnCommentReceived;
+
+        Task StartAsync();
+        Task StopAsync();
+        Task SendCommentAsync(Guid noteId, Guid userId, string title, string fullName, DateTime createdAt, string content);
+    }
+}
