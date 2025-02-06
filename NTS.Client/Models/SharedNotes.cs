e91a61aa0d35fe6ed3584ff66f91285fe96e3824
guiller santos <guillersantos111@gmail.com>
2025-02-06 22:22:26 +0800

    Fixed SharedNotesService Request Methods

--- NTS.Client/Models/SharedNotes.cs
+++ NTS.Client/Models/SharedNotes.cs
@@ -2,11 +2,11 @@
 {
     public class SharedNotes
     {
-        public Guid FavoriteNoteId { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string FullName { get; set; } = string.Empty;
+        public Guid SharedNoteId { get; set; }
         public Guid NoteId { get; set; }
         public Guid UserId { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public string Color { get; set; } = string.Empty;
