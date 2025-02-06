6e1416496ba324626d3c84519d3955fa64f9b330
guiller santos <guillersantos111@gmail.com>
2025-02-06 22:34:48 +0800

    Added new Property

--- NTS.Client/Models/Comment.cs
+++ NTS.Client/Models/Comment.cs
@@ -3,11 +3,13 @@
     public class Comment
     {
         public Guid CommentId { get; set; }
+        public Guid SharedNoteId { get; set; }
         public Guid NoteId { get; set; }
         public Guid UserId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
