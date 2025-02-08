edff7f30c77c63f637b3206d0522dab4844e2c68
guiller santos <guillersantos111@gmail.com>
2025-02-08 23:51:56 +0800

    Fixed CommentContent Property

--- NTS.Client/Models/Comment.cs
+++ NTS.Client/Models/Comment.cs
@@ -6,10 +6,8 @@
         public Guid SharedNoteId { get; set; }
         public Guid NoteId { get; set; }
         public Guid UserId { get; set; }
-        public string Title { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
-        public string Content { get; set; } = string.Empty;
-        public string Color { get; set; } = string.Empty;
+        public string CommentContent { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
