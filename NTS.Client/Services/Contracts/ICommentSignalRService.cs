d0d292d372d28c2e5a5d27043ec2cc5616a48e7c
guiller santos <guillersantos111@gmail.com>
2025-02-08 23:49:27 +0800

    Fixed Comment Service Parameters

--- NTS.Client/Services/Contracts/ICommentSignalRService.cs
+++ NTS.Client/Services/Contracts/ICommentSignalRService.cs
@@ -4,10 +4,10 @@ namespace NTS.Client.Services.Contracts
 {
     public interface ICommentSignalRService
     {
-        event Action<Guid, Guid, string, string, string, DateTime> OnCommentReceived;
+        event Action<Guid, Guid, Guid, string, string, DateTime> OnCommentReceived;
 
         Task StartAsync();
         Task StopAsync();
-        Task SendCommentAsync(Guid noteId, Guid userId, string title, string fullName, DateTime createdAt, string content);
+        Task SendCommentAsync(Guid noteId, Guid userId, Guid sharedNoteId, string fullName, DateTime createdAt, string commentContent);
     }
 }
