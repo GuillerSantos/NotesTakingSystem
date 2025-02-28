90e374f3949b6cd3206dac738998c4b51824d534
guiller santos <guillersantos111@gmail.com>
2025-02-28 22:45:28 +0800

    Improved and Fix Bug(Not Done Yet Comment Feature Not Working Properly Yet)

--- NTS.Client/Services/CommentsService.cs
+++ NTS.Client/Services/CommentsService.cs
@@ -14,10 +14,11 @@ namespace NTS.Client.Services
             this.logger = logger;
         }
 
-
         public event Action<Guid, Guid, Guid, string, string, DateTime>? OnCommentReceived;
 
-
+        /// <summary>
+        /// Sends a comment to the backend and invokes event handlers on success.
+        /// </summary>
         public async Task SendCommentAsync(Guid noteId, Guid userId, Guid sharedNoteId, string fullName, DateTime createdAt, string commentContent)
         {
             try
@@ -46,7 +47,9 @@ namespace NTS.Client.Services
             }
         }
 
-
+        /// <summary>
+        /// Retrieves a list of comments for a specific note.
+        /// </summary>
         public async Task<List<Comment>> GetCommentsForNoteAsync(Guid noteId)
         {
             try
@@ -69,11 +72,9 @@ namespace NTS.Client.Services
             return new List<Comment>();
         }
 
-
         public Task StartAsync()
         {
             throw new NotImplementedException("StartAsync logic is not yet implemented.");
         }
     }
-
 }
