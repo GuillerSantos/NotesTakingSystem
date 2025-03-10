4d4b5d5abdee39853964d84305923778d26cda2c
guiller santos <guillersantos111@gmail.com>
2025-03-10 17:06:16 +0800

    Improved Code Readability and Avoid Redundancy

--- NTS.Server/Controllers/SharedNotesController.cs
+++ NTS.Server/Controllers/SharedNotesController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NTS.Server.Services.Contracts;
-using System.Security.Claims;
+using NTS.Server.Utilities;
 
 namespace NTS.Server.Controllers
 {
@@ -29,63 +29,47 @@ namespace NTS.Server.Controllers
         [HttpPost("mark-shared/{noteId}"), Authorize(Roles = "DefaultUser")]
         public async Task<IActionResult> MarkNoteAsSharedAsync(Guid noteId)
         {
-            try
+            if (!UserClaimUtil.TryGetUserId(User, out Guid userId))
             {
-                var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
-                var userId = Guid.Parse(userIdClaim!.Value);
-                var markedNote = await sharedNotesService.MarkNoteAsSharedAsync(noteId, userId);
-
-                if (!markedNote)
-                    return BadRequest("Note Not Found Or You Are Not Authorized To Mark This Note As Shared");
-
-                return Ok($"Note Marked As Favorite {markedNote}");
+                return Unauthorized("User ID not found.");
             }
-            catch (Exception error)
-            {
-                return BadRequest(error.Message);
-            }
+
+            var markedNote = await sharedNotesService.MarkNoteAsSharedAsync(noteId, userId);
+
+            if (!markedNote)
+                return BadRequest("Note Not Found Or You Are Not Authorized To Mark This Note As Shared");
+
+            return Ok($"Note Marked As Favorite {markedNote}");
         }
 
         [HttpDelete("unmark-sharednote/{noteId}"), Authorize(Roles = "DefaultUser")]
         public async Task<IActionResult> UnmarkNoteAsSharedAsync(Guid noteId)
         {
-            try
+            if (!UserClaimUtil.TryGetUserId(User, out Guid userId))
             {
-                var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
-                if (userIdClaim == null) return Unauthorized(new { Message = "User Not Authorized." });
+                return Unauthorized("User ID not found.");
+            }
 
-                await sharedNotesService.UnmarkNoteAsSharedAsync(noteId);
-                return Ok("Note Unmarked As Shared");
-            }
-            catch (Exception error)
-            {
-                return BadRequest($"Error Unmarking Note As Shared: {error.Message}");
-            }
+            await sharedNotesService.UnmarkNoteAsSharedAsync(noteId);
+            return Ok("Note Unmarked As Shared");
         }
 
         [HttpGet("get-all-shared-notes"), Authorize(Roles = "DefaultUser")]
         public async Task<IActionResult> GetAllSharedNotesAsync()
         {
-            try
+            if (!UserClaimUtil.TryGetUserId(User, out Guid userId))
             {
-                var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
-                if (userIdClaim == null) return Unauthorized(new { Message = "User Not Authorized." });
-
-                var userId = Guid.Parse(userIdClaim.Value);
-
-                var sharedNotes = await sharedNotesService.GetAllSharedNotesAsync();
-
-                if (sharedNotes == null || !sharedNotes.Any())
-                {
-                    return NotFound(new { Message = "No Shared Notes Found." });
-                }
-
-                return Ok(sharedNotes);
+                return Unauthorized("User ID not found.");
             }
-            catch (Exception error)
+
+            var sharedNotes = await sharedNotesService.GetAllSharedNotesAsync();
+
+            if (sharedNotes == null || !sharedNotes.Any())
             {
-                return BadRequest(new { Message = $"Error Fetching All Shared Notes: {error.Message}" });
+                return NotFound(new { Message = "No Shared Notes Found." });
             }
+
+            return Ok(sharedNotes);
         }
 
         #endregion Public Methods
