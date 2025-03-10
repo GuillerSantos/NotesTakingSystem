513b52cf50268f7649b9d9a4ab46358c45fd13a6
guiller santos <guillersantos111@gmail.com>
2025-03-10 16:52:27 +0800

    Added UserClaimUtil for User Claim

---
+++ NTS.Server/Utilities/UserClaimUtil.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace NTS.Server.Utilities
+{
+    public class UserClaimUtil
+    {
+        #region Public Methods
+
+        public static bool TryGetUserId(ClaimsPrincipal user, out Guid userId)
+        {
+            var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            return Guid.TryParse(userIdClaim, out userId);
+        }
+
+        #endregion Public Methods
+    }
+}
